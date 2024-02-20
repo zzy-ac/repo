9520f27e9a54428109685b895672bc54c4939801
zzy-ac <74285757+zzy-ac@users.noreply.github.com>
2024-02-20 18:22:15 +0800

    Update entrypoint.sh

--- create-db-and-upload-action/entrypoint.sh
+++ create-db-and-upload-action/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 init_path=$PWD
 mkdir upload_packages
 echo ok--------
-cp --no-clobber $local_path/*/*/*.tar.zst ./upload_packages/
+cp -f $local_path/*/*/*.tar.zst ./upload_packages/
 
 cd upload_packages || exit 1
 
