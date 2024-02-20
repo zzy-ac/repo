85de3257c6938e4cd7e30af093acecfd95fae549
zzy-ac <74285757+zzy-ac@users.noreply.github.com>
2024-02-20 18:37:31 +0800

    Update entrypoint.sh

--- create-db-and-upload-action/entrypoint.sh
+++ create-db-and-upload-action/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 init_path=$PWD
 mkdir upload_packages
 echo ok--------
-cp -f $local_path/*/*/*.tar.zst ./upload_packages/
+cp -f --remove-destination $local_path/*/*/*.tar.zst ./upload_packages/
 
 cd upload_packages || exit 1
 
